7fb9a49c737f4bc30f1e1ffee886b602364fbbd8
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-03-01 01:43:29 +0600

    Activity Log done

--- script/app.js
+++ script/app.js
@@ -3,12 +3,19 @@ console.log("app  js working.....");
 // get the  ID
 let taskNumbers = document.getElementById("taskNumber");
 let completeTaskNumbers = document.getElementById("completeTask");
+let historyContainer = document.getElementById("historyContainer");
 let completedBtns = document.querySelectorAll(".completedBtn");
 
 for (let i = 0; i < completedBtns.length; i++) {
   completedBtns[i].addEventListener("click", function () {
     alert("Board Updated Successfully!!");
 
+    // disable the button
+    completedBtns[i].setAttribute("disabled", "true");
+    completedBtns[i].classList.remove("text-white");
+    completedBtns[i].classList.add("disabled:bg-gray-400", "text-dark-800");
+
+    // string to number conversion
     let taskNumber = parseInt(taskNumbers.innerText);
     let completeTask = parseInt(completeTaskNumbers.innerText);
 
@@ -20,6 +27,18 @@ for (let i = 0; i < completedBtns.length; i++) {
     taskNumbers.innerText = taskNumber;
     completeTaskNumbers.innerText = completeTask;
 
+    let p = document.createElement("p");
+    p.innerHTML = `
+      <p class="bg-[#F4F7FF] rounded-md px-5 py-2 text-[16px] mb-5">
+            You have Complete The Task Add Dark Mode at 12:48:15 PM
+        </p>
+      `;
+
+    // if task is zero
+    if (taskNumber === 0) {
+      alert("All Done");
+    }
+    historyContainer.appendChild(p);
     console.log(`taskNumber: ${taskNumber}, completeTask: ${completeTask}`);
     console.log(`btn clicked..... ${i}`);
   });
