de9a2fb158b655892b48c12ac9f3e865f4bd3ec0
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-03-01 02:11:37 +0600

    dynamic title showing properly

--- script/app.js
+++ script/app.js
@@ -6,6 +6,7 @@ let completeTaskNumbers = document.getElementById("completeTask");
 let historyContainer = document.getElementById("historyContainer");
 let clearHistoryBtn = document.getElementById("clearHistoryBtn");
 let completedBtns = document.querySelectorAll(".completedBtn");
+let taskTitle = document.querySelectorAll(".taskTitle");
 
 for (let i = 0; i < completedBtns.length; i++) {
   completedBtns[i].addEventListener("click", function () {
@@ -15,6 +16,9 @@ for (let i = 0; i < completedBtns.length; i++) {
     completedBtns[i].setAttribute("disabled", "true");
     completedBtns[i].classList.remove("text-white");
     completedBtns[i].classList.add("disabled:bg-gray-400", "text-dark-800");
+
+    //current task title
+    let currentTask = taskTitle[i].innerText;
 
     // string to number conversion
     let taskNumber = parseInt(taskNumbers.innerText);
@@ -31,13 +35,13 @@ for (let i = 0; i < completedBtns.length; i++) {
     let p = document.createElement("p");
     p.innerHTML = `
       <p class="bg-[#F4F7FF] rounded-md px-5 py-2 text-[16px] mb-5">
-            You have Complete The Task Add Dark Mode at ${showCurrentTime()}
+            You have Complete The Task <strong> ${currentTask} </strong > at ${showCurrentTime()}
         </p>
       `;
 
     // if task is zero
     if (taskNumber === 0) {
-      alert("All Done");
+      alert("congrats!!! You have completed all the task");
     }
     historyContainer.appendChild(p);
   });
